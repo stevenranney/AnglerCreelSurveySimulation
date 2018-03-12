cc281da3d7e6b6699b4090b92079af9de394fb27
Steven H. Ranney <steven.ranney@gmail.com>
2018-03-12 15:33:43 -0600

    Another expected test.

--- tests/testthat/test_calculate_rse.R
+++ tests/testthat/test_calculate_rse.R
@@ -15,3 +15,15 @@ test_that("calculate_rse() errors when length(x) < 2)", {
   expect_error(calculate_rse(x))
   
 })
+
+test_that("calculate_rse() returns appropriate output", {
+  
+  x <- c(2, 4)
+  
+  expected <- ((sd(x))/sqrt(length(x)))/mean(x)
+
+  expect_equal(calculate_rse(x), expected)
+  
+})
+
+
