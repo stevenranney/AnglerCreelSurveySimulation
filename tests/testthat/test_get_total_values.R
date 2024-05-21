a35e34090087b68c3ebe436d5325825cb544b20a
Steven H. Ranney <steven.ranney@gmail.com>
2024-05-21 15:07:42 -0600

    update tests for get_total_values

--- tests/testthat/test_get_total_values.R
+++ tests/testthat/test_get_total_values.R
@@ -1,42 +1,58 @@
 context("Tests of get_total_values()")
 
-test_that("Wait time passed to fx is equal to actual wait time", {
+test_that("Wait time passed to fx is equal to returned wait time", {
   
   anglers <- make_anglers()
   
+  start_time <- 2
   wait_time <- 4
   mean_catch_rate <- 3
   
-  vals <- get_total_values(anglers, wait_time = wait_time, mean_catch_rate = mean_catch_rate)
+  vals <- get_total_values(anglers, 
+                           start_time = start_time, 
+                           wait_time = wait_time, 
+                           mean_catch_rate = mean_catch_rate, 
+                           fishing_day_length = 12)
   
   expect_equal(vals$wait_time , wait_time)
   
   })
 
 test_that("When wait time is zero, effort, completed trips, and catch is 0", {
-  
+
   anglers <- make_anglers()
-  
+
+  start_time <- 2
   wait_time <- 0
   mean_catch_rate <- 3
-  
-  vals <- get_total_values(anglers, wait_time = wait_time, mean_catch_rate = mean_catch_rate)
-  
+
+  vals <- get_total_values(anglers,
+                           start_time = start_time,
+                           wait_time = wait_time,
+                           mean_catch_rate = mean_catch_rate,
+                           fishing_day_length = 12)
+
   expect_equal(is.na(vals$total_observed_trip_effort), TRUE)
-  expect_equal(vals$n_completed_trips, wait_time)
-  expect_equal(vals$total_completed_trip_effort, wait_time)
-  expect_equal(vals$total_completed_trip_catch, wait_time)
-  
+  expect_equal(vals$n_completed_trips, 0)
+  expect_equal(vals$total_completed_trip_effort, 0)
+  expect_equal(vals$total_completed_trip_catch, 0)
+
 })
 
 test_that("True effort is the same as the sum of anglers$trip_length", {
-  
+
   anglers <- make_anglers()
-  
+
+  start_time <- 2
+  wait_time <- 2
   mean_catch_rate <- 3
-  
-  vals <- get_total_values(anglers, mean_catch_rate = mean_catch_rate)
-  
+
+  vals <- get_total_values(anglers,
+                           start_time = start_time,
+                           wait_time = wait_time,
+                           mean_catch_rate = mean_catch_rate,
+                           fishing_day_length = 12)
+
   expect_equal(sum(anglers$trip_length), vals$true_effort)
 
 })
