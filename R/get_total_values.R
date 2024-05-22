d8e2eb10eb54649bcfe154da92e56101a32ccd95
Steven H. Ranney <steven.ranney@gmail.com>
2024-05-22 10:28:24 -0600

    updating examples in documenation.

--- R/get_total_values.R
+++ R/get_total_values.R
@@ -70,22 +70,25 @@
 #' library(dplyr)   
 #' set.seed(256)
 #'
-#' start_time = .001 #start of fishing day
-#' end_time = 12 #end of fishing day
-#' mean_catch_rate = 0.1 #this will cause VERY few fish to be caught!
+#' start_time <- .001 #start of fishing day
+#' end_time <- 12 #end of fishing day
+#' mean_catch_rate <- 0.1 #this will cause VERY few fish to be caught!
+#' fishing_day_length <- 12
 #' 
 #' make_anglers(100) %>%  
 #'   get_total_values(start_time = start_time, 
-#'                    end_time = end_time, mean_catch_rate = mean_catch_rate)
+#'                    end_time = end_time, mean_catch_rate = mean_catch_rate
+#'                    fishing_day_length = fishing_day_length)
 #' 
-#' start_time = .001 #start of fishing day
-#' end_time = 6 #halfway through the fishing day
-#' sampling_prob = .5 #this needs to be .5 because we are sampling only 50% of the fishing day
-#' mean_catch_rate = 0.1 #this will cause VERY few fish to be caught!
+#' start_time <- .001 #start of fishing day
+#' end_time <- 6 #halfway through the fishing day
+#' mean_catch_rate <- 0.1 #this will cause VERY few fish to be caught!
+#' fishing_day_length <- 12
 #' 
 #' make_anglers(100) %>%  
 #'   get_total_values(start_time = start_time, end_time = end_time, 
-#'                    sampling_prob = sampling_prob, mean_catch_rate = mean_catch_rate)
+#'                    mean_catch_rate = mean_catch_rate, 
+#'                    fishing_day_length = fishing_day_length)
 #'                    
 #' @export
 #' @importFrom stats sd rgamma runif
